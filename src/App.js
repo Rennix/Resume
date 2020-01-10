ed66e25973d307667d07fc745a0d5001f7a3ee72
Zac Skinner <vash077@gmail.com>
2020-01-09 20:58:27 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+// import chevron from "./chevron-down.svg"
+import chevron from "./img/chevron-down.svg"
+
+function App() {
+  return (
+    <div>
+      <div className="mainWrapper">
+        <div className="main parallax me">
+          <div className="flex flex-1"></div>
+          Zac Skinner
+          <div className="hr"></div>
+          Frontend/Software Developer
+          <div className="flex flex-1"></div>
+          <img src={chevron} className="chevron" />
+        </div>
+        <div className="jobs flex flex-column bg">
+          <div className="flex flex-row flex-around flex-fill">
+            <div className="job vorpal"></div>
+            <div className="job jpl"></div>
+            <div className="job skills">Skills and Other</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
